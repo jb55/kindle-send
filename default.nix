f53fdc6505c71138948641fdb2dd9113db034843
William Casarin <jb55@jb55.com>
2018-10-03 19:06:26 -0700

    add nix

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ stdenv
+, fetchFromGitHub
+, calibre
+, pandoc
+, makeWrapper
+}:
+let buildInputs = [ pandoc calibre ];
+    buildPaths = sep: fmt:
+      "${stdenv.lib.concatStringsSep sep (map fmt buildInputs)}";
+    paths = "${buildPaths ":" (f: "${f}/bin")}";
+in
+stdenv.mkDerivation rec {
+  name = "kindle-send-${version}";
+  version = "0.1";
+
+  src = ./.;
+
+  buildInputs = [ makeWrapper ];
+
+  phases = [ "unpackPhase" "installPhase" ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    install kindle-{email,email-txt,send,web} $out/bin
+
+    for file in $out/bin/*
+    do
+      wrapProgram "$file" --prefix PATH : ${paths}
+    done
+  '';
+
+}
